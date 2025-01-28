f61891a6132ed5a3998a869e6c8071b1785d4138
Fernando Fernández <ferferga@hotmail.com>
2025-01-28 06:20:02 +0000

    refactor(docker): start testing with test-network, reintroduce multi-stage, some compat tools

--- docker-build/scripts/postunpack.sh
+++ docker-build/scripts/postunpack.sh
@@ -3,18 +3,31 @@
 set -e
 
 echo "=== Bootstrapping Hyperledger... ==="
-curl https://raw.githubusercontent.com/hyperledger/fabric/refs/heads/main/scripts/bootstrap.sh \
-  | bash -s -- $1 $2 -s -b
-#  | bash -s -- $1 $2
+# If downloading the binaries, curl is required as a prerequisite of the image
+WGETRC=/dev/null wget -q -O - https://raw.githubusercontent.com/hyperledger/fabric/refs/heads/main/scripts/bootstrap.sh \
+  | bash -s -- $1 $2
+#  | bash -s -- $1 $2 -s -b
 
-#mkdir -p /fabric/bin /fabric/config
-#cp -r fabric-samples/bin /fabric
-#cp -r fabric-samples/config /fabric
-#mv fabric-samples/config fabric-samples/configtx
+mkdir -p "${FABRIC_TOOLS_PATH}"/bin "${FABRIC_TOOLS_PATH}"/config "${CHAINCODE_PATH}"
+cp -r fabric-samples/bin ${FABRIC_TOOLS_PATH}
+cp -r fabric-samples/config ${FABRIC_TOOLS_PATH}
+mv fabric-samples/test-network ${FABRIC_TOOLS_PATH}
+rm -rf fabric-samples
+
+# TODO: Investigate which images can be scrapped
+echo
+echo "=== Pulling extra images... ==="
+# Used by monitorDocker.sh
+docker pull gliderlabs/logspout
+# Used by network.sh down
+docker pull busybox
+# Used as the channel's database
+docker pull couchdb:3.3.3
 
 echo
 echo "=== Saving images... ==="
-docker_ids=$(/usr/local/bin/docker images --filter "label=${DIND_DEFAULT_LABEL}" --format "{{.ID}}" | sort -u)
+#docker_ids=$(/usr/local/bin/docker images --filter "label=${DIND_DEFAULT_LABEL}" --format "{{.ID}}" | sort -u)
+docker_ids=$(/usr/local/bin/docker images --format "{{.ID}}" | sort -u)
 
 for id in $docker_ids; do
   (
