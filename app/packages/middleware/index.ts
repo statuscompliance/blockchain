899058aa8257303d8c51ccaf13642855af78fad2
Fernando Fernández <ferferga@hotmail.com>
2025-01-28 06:16:59 +0000

    refactor: remove dind labeling
    Previously, for running DIND, we had to mount the host's Docker socket inside the container, which meant thatall the resources created inside the Docker container would be exposed to the host.
    
    However, it's possible to run docker in docker completely isolated from the host (in the sense that it's not needed to share any resources for it, since privileged it's still needed), so all the handling logic for removing an specific label and labeling all resources upon creation is no longer needed.

--- app/packages/middleware/index.ts
+++ app/packages/middleware/index.ts
@@ -1,27 +1,8 @@
-import { execSync } from 'node:child_process';
 import { logger } from '@statuscompliance/blockchain-shared/logger';
 import Fastify from 'fastify';
 import { routes } from './src/routes/index.ts';
 
 console.info('Starting status ledger...');
-
-/**
- * This function kills the hyperledger containers when the process is terminated
- */
-function onExit(): void {
-  console.info('Cleaning up hyperledger containers...');
-  execSync('docker ps -a --filter "label=$DIND_DEFAULT_LABEL" -q | xargs docker rm -f || true', { stdio: 'ignore' });
-  execSync('docker images --filter "label=$DIND_DEFAULT_LABEL" -q | xargs docker rmi -f || true', { stdio: 'ignore' });
-  execSync('docker network ls --filter "label=$DIND_DEFAULT_LABEL" -q | xargs docker network rm -f || true', { stdio: 'ignore' });
-  execSync('docker volume ls --filter "label=$DIND_DEFAULT_LABEL" -q | xargs docker volume rm -f || true', { stdio: 'ignore' });
-  logger.success('Cleanup done! Exiting...\n');
-}
-
-// process.on('SIGTERM', onExit);
-// process.on('SIGINT', onExit);
-// process.on('exit', onExit);
-// process.on('unhandledRejection', onExit);
-// process.on('uncaughtException', onExit);
 
 const app = Fastify();
 
@@ -39,7 +20,7 @@ await app.ready();
 app.swagger();
 
 try {
-  app.listen({
+  await app.listen({
     port: 3000,
     listenTextResolver: (address: string) => {
       logger.info(`Server listening at ${address}`);
@@ -48,5 +29,4 @@ try {
   });
 } catch (error) {
   app.log.error(error);
-  process.exit(1);
 }
